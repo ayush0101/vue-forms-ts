b06db71cb84f4c5f5b1fca18c1544e59abb84cfb
Vivekanand P V <vivekanandpv@outlook.com>
2021-01-22 11:35:49 +0530

    refactoring and dropdown

--- src/components/NormalForm.vue
+++ src/components/NormalForm.vue
@@ -2,28 +2,41 @@
   <form @submit.prevent="onSubmit">
     <div class="form-group">
       <label>Email address</label>
-      <input type="email" class="form-control" v-model="email" />
+      <input type="email" class="form-control" v-model="registration.email" />
       <small class="form-text text-muted"
         >We'll never share your email with anyone else.</small
       >
     </div>
     <div class="form-group">
       <label>Password</label>
-      <input type="password" class="form-control" v-model="password" />
+      <input
+        type="password"
+        class="form-control"
+        v-model="registration.password"
+      />
     </div>
     <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" v-model="checkMeOut" />
+      <input
+        type="checkbox"
+        class="form-check-input"
+        v-model="registration.checkMeOut"
+      />
       <label class="form-check-label">Check me out</label>
     </div>
     <div class="form-group">
       <label>Example textarea</label>
-      <textarea class="form-control" rows="3" v-model="example"></textarea>
+      <textarea
+        class="form-control"
+        rows="3"
+        v-model="registration.example"
+      ></textarea>
     </div>
 
     <div class="form-group">
       <label>Country</label>
-      <select class="form-control" v-model="country">
-        <option>Default select</option>
+      <select class="form-control" v-model="registration.country">
+        <option value="">--- Please Select ---</option>
+        <option v-for="c in countries" :key="c" :value="c">{{ c }}</option>
       </select>
     </div>
 
@@ -34,7 +47,7 @@
         type="radio"
         name="inlineRadioOptions"
         value="Every day"
-        v-model="exercise"
+        v-model="registration.exercise"
       />
       <label>Every day</label>
     </div>
@@ -44,7 +57,7 @@
         type="radio"
         name="inlineRadioOptions"
         value="Three times in a week"
-        v-model="exercise"
+        v-model="registration.exercise"
       />
       <label>Three times in a week</label>
     </div>
@@ -54,7 +67,7 @@
         type="radio"
         name="inlineRadioOptions"
         value="Weekly once"
-        v-model="exercise"
+        v-model="registration.exercise"
       />
       <label>Weekly once</label>
     </div>
@@ -70,23 +83,26 @@ import { defineComponent } from "vue";
 export default defineComponent({
   data() {
     return {
-      email: "",
-      password: "",
-      checkMeOut: false,
-      example: "",
-      country: "",
-      exercise: "",
+      registration: {
+        email: "",
+        password: "",
+        checkMeOut: false,
+        example: "",
+        country: "",
+        exercise: "",
+      },
+      countries: ["India", "USA", "Japan", "UK", "France"],
     };
   },
   methods: {
     onSubmit() {
       const formData = {
-        email: this.email,
-        password: this.password,
-        checkMeOut: this.checkMeOut,
-        example: this.example,
-        country: this.country,
-        exercise: this.exercise,
+        email: this.registration.email,
+        password: this.registration.password,
+        checkMeOut: this.registration.checkMeOut,
+        example: this.registration.example,
+        country: this.registration.country,
+        exercise: this.registration.exercise,
       };
 
       console.log(formData);
